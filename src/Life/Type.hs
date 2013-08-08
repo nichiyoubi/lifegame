3566a64101d839d8f7abd7d4dcbc480779624b4e
宇佐美 雅紀 <usamimasanori@usamimac.lan>
2013-08-09 01:30:22 +0900

    add test

--- src/Life/Type.hs
+++ src/Life/Type.hs
@@ -1,4 +1,8 @@
-data Lifegame = Lifegame { size : Int
-			 , board : [(Int, Int)]
-			 }
+module Life.Type
+(Lifegame(..))
+where
+
+data Lifegame = Lifegame { size :: Int
+			 , board :: [(Int, Int)]
+			 } deriving (Eq, Show)
 
---
+++ src/Test/LifeTest.hs
@@ -0,0 +1,10 @@
+-- module Test.LifeTest where
+
+import Life.Type
+-- import Life.CountAlive
+-- import Test.HUnit
+
+game = Lifegame { size = 3, board = []}
+
+-- test1 = TestCase(assertEqual "countAlive game1," 0 (countAlive game1))
+-- tests = TestList[TestLavel "test1" test1]
