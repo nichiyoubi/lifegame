00a0b920433cc8217b0759aba95f9408aebfc034
U-ESOL\m-usami <m-usami@m-usami.ESOL.CO.JP>
2013-08-15 12:10:47 +0900

    complete countAlive

--- src/Life/CountAlive.hs
+++ src/Life/CountAlive.hs
@@ -3,7 +3,25 @@ module Life.CountAlive(countAlive) where
 import Life.Type
 
 countAlive :: (Int, Int) -> Lifegame -> Int 
-countAlive _ _ = 0
+countAlive (posx, posy) (Lifegame size board) = 
+			  isAlive' (posx-1, posy-1) board 
+			+ isAlive' (posx  , posy-1) board 
+			+ isAlive' (posx+1, posy-1) board 
+			+ isAlive' (posx-1, posy  ) board 
+			+ isAlive' (posx+1, posy  ) board 
+			+ isAlive' (posx-1, posy+1) board 
+			+ isAlive' (posx  , posy+1) board 
+			+ isAlive' (posx+1, posy+1) board 
 
-countAlive' :: (Int, Int) -> Lifegame -> Int
-countAlive' pos gamme
+isAlive :: (Int, Int) -> [(Int, Int)] -> Int
+isAlive _ [] = 0
+isAlive (x, y) ((cellx, celly):cells) = if ((x==cellx) && (y==celly))
+					then 1
+					else isAlive (x, y) cells
+
+isAlive' :: (Int, Int) -> [(Int, Int)] -> Int
+isAlive' _ [] = 0
+isAlive' pos cells = if elem pos cells
+			then 1
+			else 0
+
