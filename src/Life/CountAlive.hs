bf9c86b739281606fc9808349769fbfcceb598f3
U-ESOL\m-usami <m-usami@m-usami.ESOL.CO.JP>
2013-08-15 13:38:01 +0900

    complete countAlive

--- src/Life/CountAlive.hs
+++ src/Life/CountAlive.hs
@@ -2,8 +2,8 @@ module Life.CountAlive(countAlive) where
 
 import Life.Type
 
-countAlive :: (Int, Int) -> Lifegame -> Int 
-countAlive (posx, posy) (Lifegame size board) = 
+countAlive' :: (Int, Int) -> Lifegame -> Int 
+countAlive' (posx, posy) (Lifegame size board) = 
 			  isAlive' (posx-1, posy-1) board 
 			+ isAlive' (posx  , posy-1) board 
 			+ isAlive' (posx+1, posy-1) board 
@@ -12,6 +12,13 @@ countAlive (posx, posy) (Lifegame size board) =
 			+ isAlive' (posx-1, posy+1) board 
 			+ isAlive' (posx  , posy+1) board 
 			+ isAlive' (posx+1, posy+1) board 
+
+countAlive :: (Int, Int) -> Lifegame -> Int
+countAlive (posx, posy) (Lifegame size board) =
+			sum (map (isAlive'' board) poses)
+			where poses = [(x, y)| x<-[posx-1..posx+1],
+					       y<-[posy-1..posy+1],
+					       ((x==posx)&&(y==posy))/=True]
 
 isAlive :: (Int, Int) -> [(Int, Int)] -> Int
 isAlive _ [] = 0
@@ -25,3 +32,8 @@ isAlive' pos cells = if elem pos cells
 			then 1
 			else 0
 
+isAlive'' :: [(Int, Int)] -> (Int, Int) -> Int
+isAlive'' [] _ = 0
+isAlive'' cells pos = if elem pos cells
+			then 1
+			else 0
